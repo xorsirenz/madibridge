8aa46c9ffb16e13277080ba1f69009498c5048de
xorsirenz <69394836+xorsirenz@users.noreply.github.com>
2026-05-12 10:37:51 -0700

    added args for help/version

---
+++ internal/utils/handle_cmds.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func HandleCmd(version string) {
+	hFlag := flag.Bool("help", false, "Show help")
+	vFlag := flag.Bool("version", false, "Print version")
+	flag.BoolVar(hFlag, "h", *hFlag, "alias for -help")
+	flag.BoolVar(vFlag, "v", *vFlag, "alias for -version")
+	flag.Parse()
+
+	if *vFlag {
+		fmt.Println("madibridge", version)
+		os.Exit(0)
+	}
+	if *hFlag {
+		fmt.Println(`Usage:
+	madibridge
+
+Operations:
+	madibridge {-h -help}
+	madibridge {-v -version}`)
+		os.Exit(0)
+	}
+}
