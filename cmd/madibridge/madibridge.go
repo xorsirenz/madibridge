e363729274a402fe4ee73869a99aa7408adceccd
xorsirenz <69394836+xorsirenz@users.noreply.github.com>
2026-05-12 10:37:34 -0700

    check args for help/version

--- cmd/madibridge/madibridge.go
+++ cmd/madibridge/madibridge.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/xorsirenz/madibridge/internal/bridge"
 	"github.com/xorsirenz/madibridge/internal/config"
+	"github.com/xorsirenz/madibridge/internal/utils"
 )
 
 var version string
 
 func main() {
+	if len(os.Args) > 1 {
+		utils.HandleCmd(version)
+	}
+
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatal("failed to load config:", err)
